8f5b05834e209ddfee3260df08dfa99898adc03a
Nachopm98 <32020695+Nachopm98@users.noreply.github.com>
2021-09-29 08:34:52 +0200

    Create permissions.js

---
+++ www/permissions.js
@@ -0,0 +1,41 @@
+function Permissions (){
+
+  this.ACCESS_FINE_LOCATION = 'android.permission.ACCESS_FINE_LOCATION';
+
+}
+
+function deprecated(name) {
+  console.warn("Calling cordova.plugins.permissions." + name + " with the successCallback as first argument is deprecated");
+  console.warn("The new signature is '" + name + "(permission, successCallback, errorCallback)'");
+}
+Permissions.prototype = {
+    checkPermission: function(permission, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, permissionsName, 'checkPermission', [permission]);
+    },
+    requestPermission: function(permission, successCallback, errorCallback) {
+        if (typeof permission === "function") {
+            deprecated("requestPermission");
+            successCallback = arguments[0];
+            errorCallback = arguments[1];
+            permission = arguments[2];
+        }
+        cordova.exec(successCallback, errorCallback, permissionsName, 'requestPermission', [permission]);
+    },
+    requestPermissions: function(permissions, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, permissionsName, 'requestPermissions', permissions);
+    }
+};
+
+Permissions.prototype.hasPermission = function (permission, successCallback, errorCallback) {
+    console.warn("hasPermission() function deprecated. Considers using checkPermission()");
+
+    if (typeof permission === "function") {
+        deprecated("hasPermission");
+        successCallback = arguments[0];
+        errorCallback = arguments[1];
+        permission = arguments[2];
+    }
+    this.checkPermission.call(this, permission, successCallback, errorCallback);
+};
+
+module.exports = new Permissions();
